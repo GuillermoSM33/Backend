83fde566a2d4337fb5c3417f007ef86e42f5912b
GuillermoSM33 <guillermo.jesus.garcia.canul@gmail.com>
2024-09-19 00:44:44 -0500

    Se agrega lógica para el backend

--- backend/Models/UsuariosContext.cs
+++ backend/Models/UsuariosContext.cs
@@ -18,7 +18,7 @@ public partial class UsuariosContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=ALI_G;Database=Usuarios;User Id=Guillermo_G;Password=Alisson240904;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=Localhost;Database=Usuarios;User Id=Guillermo_G;Password=Alisson240904;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -26,7 +26,7 @@ public partial class UsuariosContext : DbContext
         {
             entity.ToTable("usuarios");
 
-            entity.Property(e => e.EsEstudiante).HasColumnName("Es_estudiante");
+            entity.Property(e => e.EsEstudiante).HasColumnName("esEstudiante");
             entity.Property(e => e.Nombre).HasMaxLength(100);
         });
 
